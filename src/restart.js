b39fecbdc26eda47c8b1c252024a6afa3e456623
Stephan Bönnemann <stephan@boennemann.me>
2015-02-02 17:39:34 +0100

    fix(restart): correctly prevent double `npm publish` run

--- src/restart.js
+++ src/restart.js
@@ -2,7 +2,7 @@
 
 var spawn = require('child_process').spawn
 
-module.exports = function () {
+var exports = module.exports = function (cb) {
   // npm loads package.json data before running the `prepublish` hook
   // changing the version on `prepublish` has no effect
   // see https://github.com/npm/npm/issues/7118
@@ -10,7 +10,21 @@ module.exports = function () {
   // the package.json is then loaded again and the correct version will be published
 
   var child = spawn('npm', ['publish', '--semantic-release-rerun'])
+  var handler = exports.handleCloseAndExit.bind(null, cb)
 
   child.stdout.pipe(process.stdout)
   child.stderr.pipe(process.stderr)
+
+  child.on('close', handler)
+  child.on('exit', handler)
+  child.on('error', cb)
+}
+
+exports.handleCloseAndExit = function (cb, code, signal) {
+  if (code === 0) return cb(null)
+  cb({
+    code: code,
+    signal: signal,
+    message: 'npm publish failed'
+  })
 }
