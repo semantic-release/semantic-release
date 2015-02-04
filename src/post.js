7380d586ee0f0e80e2dd8341a849cf64f58dc954
Christoph Witzko <github@christophwitzko.com>
2015-02-04 23:31:56 +0100

    feat(release): configure github api endpoint via environment variables

--- src/post.js
+++ src/post.js
@@ -2,6 +2,7 @@
 
 var exec = require('child_process').exec
 var readFile = require('fs').readFileSync
+var url = require('url')
 
 var changelog = require('conventional-changelog')
 var GitHubApi = require('github')
@@ -10,15 +11,20 @@ var parseUrl = require('github-url-from-git')
 
 var efh = require('../lib/error').efh
 
-var github = new GitHubApi({
-  version: '3.0.0'
-})
-
 module.exports = function (options, cb) {
   var pkg = JSON.parse(readFile('./package.json'))
   var repository = pkg.repository ? pkg.repository.url : null
 
   if (!repository) return cb(new Error('Package must have a repository'))
+
+  var config = options['github-url'] ? url.parse(options['github-url']) : {}
+
+  var github = new GitHubApi({
+    version: '3.0.0',
+    port: config.port,
+    protocol: (config.protocol || '').split(':')[0] || null,
+    host: config.hostname
+  })
 
   changelog({
     version: pkg.version,
