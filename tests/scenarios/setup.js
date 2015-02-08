44eb633458ced98105ab039a0568f3f39b97776e
Stephan Bönnemann <stephan@boennemann.me>
2015-02-08 16:29:27 +0100

    test(setup): setup package.json

---
+++ tests/scenarios/setup.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var join = require('path').join
+var readFile = require('fs').readFileSync
+
+var efh = require('error-first-handler')
+var nixt = require('nixt')
+
+module.exports = function (test, createModule) {
+  createModule({
+    repository: {},
+    scripts: {
+      postpublish: 'npm run gh-pages'
+    }
+  }, efh()(function (name, cwd) {
+    test('setup', function (t) {
+      t.test('setup "package.json"', function (t) {
+        t.plan(5)
+
+        nixt()
+          .cwd(cwd)
+          .exec('git remote add origin git@github.com:user/repo.git')
+          .run('./node_modules/.bin/semantic-release setup')
+          .code(0)
+          .end(function(err) {
+            t.error(err, 'nixt')
+
+            var pkg = JSON.parse(readFile(join(cwd, 'package.json')))
+
+            t.is(pkg.version, '0.0.0-semantically-released', 'version')
+            t.is(pkg.repository.url, 'https://github.com/user/repo', 'repo')
+            t.is(pkg.scripts.prepublish, 'semantic-release pre', 'pre')
+            t.is(pkg.scripts.postpublish, 'npm run gh-pages && semantic-release post', 'post')
+          })
+      })
+    })
+  }))
+}
