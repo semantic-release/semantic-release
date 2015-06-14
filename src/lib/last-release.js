f62525ad7ee5cded7a1b18b6266e65409ac6c13a
Stephan Bönnemann <stephan@boennemann.me>
2015-06-14 11:24:11 -0700

    feat(last-release): initial functionality

---
+++ src/lib/last-release.js
@@ -0,0 +1,21 @@
+const npmlog = require('npmlog')
+const RegClient = require('npm-registry-client')
+
+module.exports = function (pkg, npmConfig, cb) {
+  npmlog.level = npmConfig.loglevel || 'error'
+  const client = new RegClient({log: npmlog})
+
+  client.get(`${npmConfig.registry}/${pkg.name}`, {
+    auth: npmConfig.auth
+  }, (err, data) => {
+    if (err && err.statusCode === 404) return cb(null, {})
+    if (err) return cb(err)
+
+    const version = data['dist-tags'].latest
+
+    cb(null, {
+      version,
+      gitHead: data.versions[version].gitHead
+    })
+  })
+}
