f79e26dcd6a86d1300a35f43b8daa8f6aad366b0
Stephan Bönnemann <stephan@boennemann.me>
2015-06-14 18:40:44 -0700

    test(type): initial suite

---
+++ test/specs/type.js
@@ -0,0 +1,44 @@
+const test = require('tap').test
+
+const type = require('../../dist/lib/type.js')
+
+test('get type from commits', (t) => {
+  t.test('get type from plugin', (t) => {
+    type({
+      analyze: () => 'major'
+    }, {
+      commits: [{
+        hash: '0',
+        message: 'a'
+      }],
+      lastRelease: {
+        version: '1.0.0'
+      }
+    }, (err, type) => {
+      t.error(err)
+      t.is(type, 'major')
+    })
+  })
+
+  t.test('error when no changes', (t) => {
+    type({
+      analyze: () => null
+    }, {
+      commits: [],
+      lastRelease: {}
+    }, (err) => {
+      t.is(err.code, 'ENOCHANGE')
+    })
+  })
+
+  t.test('initial version', (t) => {
+    type({}, {
+      lastRelease: {
+        version: null
+      }
+    }, (err, type) => {
+      t.error(err)
+      t.is(type, 'initial')
+    })
+  })
+})
