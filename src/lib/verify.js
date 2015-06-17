b63a43fa535ec01a266f157b8c8e5f222eef1806
Stephan Bönnemann <stephan@boennemann.me>
2015-06-17 15:42:14 -0700

    feat(verify): initial verification for pkg, options and env

---
+++ src/lib/verify.js
@@ -0,0 +1,37 @@
+const SemanticReleaseError = require('./error')
+
+module.exports = function (pkg, options, env) {
+  let errors = []
+
+  if (!pkg.name) {
+    errors.push(new SemanticReleaseError(
+      'No "name" found in package.json.',
+      'ENOPKGNAME'
+    ))
+  }
+
+  if (!pkg.repository || !pkg.repository.url) {
+    errors.push(new SemanticReleaseError(
+      'No "repository" found in package.json.',
+      'ENOPKGREPO'
+    ))
+  }
+
+  if (options.debug) return errors
+
+  if (!options['github-token']) {
+    errors.push(new SemanticReleaseError(
+      'No github token specified.',
+      'ENOGHTOKEN'
+    ))
+  }
+
+  if (!env.NPM_TOKEN) {
+    errors.push(new SemanticReleaseError(
+      'No npm token specified.',
+      'ENONPMTOKEN'
+    ))
+  }
+
+  return errors
+}
