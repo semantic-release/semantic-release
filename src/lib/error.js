a27998f92ae1563a1c9d0317c96fd0692669fdc7
Stephan Bönnemann <stephan@boennemann.me>
2015-06-17 10:03:58 -0700

    fix(error): exit with 1, so npm publish isn't run after fails; closes #45

--- src/lib/error.js
+++ src/lib/error.js
@@ -1,8 +1,9 @@
 exports.efh = require('error-first-handler')
 exports.standard = exports.efh((err) => {
   console.log('Something went wrong:')
-  if (typeof err === 'string') return console.log(err)
-  if (err instanceof Error) return console.log(err.message, err.stack)
-  if (err.message) return console.log(err.message)
-  console.log(err)
+  if (typeof err === 'string') console.log(err)
+  else if (err instanceof Error) console.log(err.message, err.stack)
+  else if (err.message) console.log(err.message)
+  else console.log(err)
+  process.exit(1)
 })
