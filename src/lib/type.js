98896ef1b5a549f5b18685be03274d32cd40db6b
Ari Porad <ari@porad.com>
2015-08-18 16:19:10 -0700

    feat(commit-analyzer): Call commit-analyzer multiple times
    type.js will now call commit-analyzer multiple times, and keep track of the
    release type itself. This makes the commit-analyzer drastically simpler.
    
    BREAKING CHANGE:
    	The commit-analyzer plugin is now called only once, and with only a single
    	commit (as the `commit` config option, vs. `commits`). The plugin also does
    	not have to keep track of the release type for multiple commits. To migrate,
    	simply have your plugin only accept and parse a single commit. This should
    	reduce the complexity a good deal.

--- src/lib/type.js
+++ src/lib/type.js
@@ -1,14 +1,16 @@
+// TODO: Test, update commit-analyzer
 const SemanticReleaseError = require('@semantic-release/error')
+const _ = require('lodash')
 
 module.exports = function (config, cb) {
-  const { plugins, lastRelease } = config
+  const { plugins, lastRelease, commits } = config
 
-  plugins.analyzeCommits(config, (err, type) => {
+  parseCommits(plugins.analyzeCommits, config, commits, (err, type) => {
     if (err) return cb(err)
 
     if (!type) {
       return cb(new SemanticReleaseError(
-        'There are no relevant changes, so no new version is released.',
+        'There are no relevant changes so no new version is released.',
         'ENOCHANGE'
       ))
     }
@@ -17,4 +19,29 @@ module.exports = function (config, cb) {
 
     cb(null, type)
   })
+}
+
+function parseCommits (parse, config, commits, cb) {
+  // 0 is none, 1 is patch, 2 is minor, 3 is major. Then we can do less than.
+  const types = ['none', 'patch', 'minor', 'major']
+  let type = 0
+
+  function nextCommit (i) {
+    parse(_.defaults({ commit: commits[i] }, config), (err, t) => {
+      if (err) return cb(err)
+      if (typeof t === 'string') t = types.indexOf(t)
+
+      if (type < t) type = t
+
+      if (i === 0) {
+        let out = types[type]
+        if (out === types[0]) out = false
+        return cb(null, out)
+      }
+
+      setImmediate(() => nextCommit(i - 1))
+    })
+  }
+
+  nextCommit(commits.length - 1)
 }
