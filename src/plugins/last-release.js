92414773d3da12bfdfd38f43e8572360c306d358
Joshua Nelson <jonelson@atlassian.com>
2016-05-25 15:13:37 +1000

    chore(build): correct logging (no warn)

--- src/plugins/last-release.js
+++ src/plugins/last-release.js
@@ -12,8 +12,8 @@ module.exports = function lastReleaseLerna (_ref, cb) {
   var gitHead = shell.exec(['git rev-list -n 1', tag].join(' '));
 
   if (gitHead.code !== 0) {
-    console.warn('Error: git head could not be calculated from the tag `' + tag + '`. Ensure this tag exists locally.');
-    console.warn('Attempting to use the first commit instead');
+    console.log('Error: git head could not be calculated from the tag `' + tag + '`. Ensure this tag exists locally.');
+    console.log('Attempting to use the first commit instead');
     gitHead = shell.exec('git rev-list --max-parents=0 HEAD');
   }
 
