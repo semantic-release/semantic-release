03620bd3eee765bb46688beaaf8d1c58e9e90629
Joshua Nelson <jonelson@atlassian.com>
2016-06-05 23:27:44 +1000

    fix(pre): fix relevant release plugin

--- src/plugins/analyzer.js
+++ src/plugins/analyzer.js
@@ -7,17 +7,20 @@ module.exports = {
     var commits = _ref.commits;
 
     var relevantCommits = commits.filter(function (commit) {
-      var affectsLine = commit.message.split('\n\n')[1];
-      return module.exports.isRelevant(affectsLine, pkg.name);
+      return module.exports.isRelevant(commit, pkg.name);
     });
 
     commitAnalyzer({}, Object.assign(_ref, {commits: relevantCommits}), function (err, type) {
       log.info('Anaylzed', relevantCommits.length, 'commits to determine type', type, 'for', pkg.name);
+      relevantCommits.length && log.info('Commits:\n', relevantCommits.map(function (commit) {
+        return commit.hash + ': ' + commit.message;
+      }).join('\n----\n'));
       cb(err, type);
     });
   },
 
-  isRelevant: function (commitBody, packageName) {
-    return commitBody && commitBody.indexOf('affects:') === 0 && packageName.indexOf(packageName) > -1
+  isRelevant: function (commit, packageName) {
+    var affectsLine = commit.message.split('\n\n')[1];
+    return affectsLine && affectsLine.indexOf('affects:') === 0 && affectsLine.indexOf(packageName) > -1
   }
 };
