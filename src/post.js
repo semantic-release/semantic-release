39686413449cd730aa6effdb3c12e791df2279e3
Joshua Nelson <jonelson@atlassian.com>
2016-06-07 16:14:04 +1000

    feat(changelog): added changelog generation
    Currently it deletes and recreated tags

--- src/post.js
+++ src/post.js
@@ -3,6 +3,8 @@ var async = require('async');
 var shell = require('shelljs');
 var fs = require('fs');
 var path = require('path');
+var simpleGit = require('simple-git');
+var semver = require('semver');
 
 var lernaPackages = require('./lerna/packages');
 var execAsTask = require('./utils/exec-as-task');
@@ -11,15 +13,76 @@ var log = require('./utils/log');
 
 var CHANGELOG_FILE_NAME = 'CHANGELOG.md';
 
+function getPkgPath (packagePath) {
+  return path.join(packagePath, 'package.json')
+}
+
+function getPkg (packagePath) {
+  return JSON.parse(fs.readFileSync(getPkgPath(packagePath)));
+}
+
+function getTagParts (tag, separator, isVersionFirst) {
+  var parts = tag.split(separator);
+  if (parts.length !== 2) {
+    return null;
+  }
+  return {
+    packageName: isVersionFirst ? parts[1] : parts[0],
+    version: isVersionFirst ? parts[0] : parts[1]
+  }
+}
+
+function isValidTag (tagParts) {
+  return tagParts && tagParts.packageName && (semver.valid(tagParts.version))
+}
+
+function makeTemporaryTag (tagParts) {
+  return tagParts.version + '-tmp-duplicate-tag-' + tagParts.packageName;
+}
+
+function makeLernaTag (tagParts) {
+  return tagParts.packageName + '@' + tagParts.version;
+}
+
+function getTemporaryTagParts (temporaryTag) {
+  return getTagParts(temporaryTag, '-tmp-duplicate-tag-', true);
+}
+
+function getLernaTagParts (lernaTag) {
+  return getTagParts(lernaTag, '@', false);
+}
+
+function addVersionToCommit (commit) {
+  var rtag = /tag:\s*[v=]?(.+?)[,\)]/gi;
+  if (commit.gitTags) {
+    console.log(commit.gitTags);
+    var match = rtag.exec(commit.gitTags);
+    rtag.lastIndex = 0;
+
+    if (!match) {
+      return;
+    }
+
+    var temporaryTag = match[1];
+    var tagParts = getTemporaryTagParts(temporaryTag);
+    if (isValidTag(tagParts)) {
+      commit.version = makeLernaTag(tagParts);
+    }
+  }
+}
+
 function createChangelog (done) {
   var packagePath = this.packagePath;
-  var pkgJsonPath = path.join(packagePath, 'package.json');
-  var writeStream = fs.createWriteStream(path.join(packagePath, CHANGELOG_FILE_NAME), {flags: 'a'});
+  var pkgJsonPath = getPkgPath(packagePath);
+  var writeStream = fs.createWriteStream(path.join(packagePath, CHANGELOG_FILE_NAME));
   var stream = conventionalChangelog({
     preset: 'angular',
     transform: function (commit, cb) {
-      var pkgJsonFile = JSON.parse(fs.readFileSync(pkgJsonPath));
+      var pkgJsonFile = getPkg(packagePath);
       if (analyzer.isRelevant(commit.body, pkgJsonFile.name)) {
+
+        addVersionToCommit(commit);
+
         cb(null, commit);
       } else {
         cb(null, null);
@@ -27,12 +90,53 @@ function createChangelog (done) {
     },
     pkg: {
       path: pkgJsonPath
-    }
+    },
+    releaseCount: 0
+  }, {
+
   }).pipe(writeStream);
 
   stream.on('close', function () {
     done(null);
   });
+}
+
+
+
+function temporarilyRenameTags (done) {
+  var packageName = getPkg(this.packagePath).name;
+  simpleGit().tags(function (err, tags) {
+    var relevantTags = tags.all.filter(function isTagRelevant (lernaTag) {
+      var tagParts = getLernaTagParts(lernaTag);
+      return isValidTag(tagParts) && tagParts.packageName === packageName;
+    });
+
+    console.log(relevantTags);
+
+    relevantTags.forEach(function renameTag (lernaTag) {
+      shell.exec('git tag ' + makeTemporaryTag(getLernaTagParts(lernaTag)) + ' ' + lernaTag);
+      shell.exec('git tag -d ' + lernaTag);
+    });
+
+    done(err);
+  });
+}
+
+function unRenameTags (done) {
+  var packageName = getPkg(this.packagePath).name;
+  simpleGit().tags(function (err, tags) {
+    var relevantTags = tags.all.filter(function isTagRelevant (temporaryTag) {
+      var tagParts = getTemporaryTagParts(temporaryTag);
+      return isValidTag(tagParts) && tagParts.packageName === packageName;
+    });
+
+    relevantTags.forEach(function unRenameTag (temporaryTag) {
+      shell.exec('git tag ' + makeLernaTag(getTemporaryTagParts(temporaryTag)) + ' ' + temporaryTag);
+      shell.exec('git tag -d ' + temporaryTag);
+    });
+
+    done(err);
+  })
 }
 
 function enterPackage (done) {
@@ -46,15 +150,17 @@ function exitPackage (done) {
 
 module.exports = function () {
   lernaPackages.forEachPackage([
+    temporarilyRenameTags,
     createChangelog,
+    unRenameTags,
     enterPackage,
     execAsTask('touch ' + CHANGELOG_FILE_NAME),
     execAsTask('git add ' + CHANGELOG_FILE_NAME),
     exitPackage
   ], {}, function done () {
     async.series([
-      execAsTask('git commit -anm\'docs(changelog): appending to changelog\' --allow-empty'),
-      execAsTask('git push origin')
+      //execAsTask('git commit -anm\'docs(changelog): appending to changelog\' --allow-empty'),
+      //execAsTask('git push origin')
     ]);
   });
 };
