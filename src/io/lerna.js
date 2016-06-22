f00642974e86dd8905ac5923ba1731a625631ef8
Joscha Feth <jfeth@atlassian.com>
2016-06-22 17:43:21 +1000

    fix: make lerna-semantic-release work with lerna 2.0.0-beta.20 and up

--- src/io/lerna.js
+++ src/io/lerna.js
@@ -1,5 +1,5 @@
-var Repository = require('lerna/lib/Repository').default;
-var PackageUtilities = require('lerna/lib/PackageUtilities').default;
+var Repository = require('lerna/lib/Repository');
+var PackageUtilities = require('lerna/lib/PackageUtilities');
 
 module.exports = {
   getAllPackages: function () {
