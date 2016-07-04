2bdf252b6889654b6c73d12df457e8b829b0da31
Joshua Nelson <jonelson@atlassian.com>
2016-07-04 14:22:35 +1000

    fix(pre): lerna-semantic-release now handles private packages

--- packages/lerna-semantic-release-io/io/semantic-release.js
+++ packages/lerna-semantic-release-io/io/semantic-release.js
@@ -2,7 +2,7 @@ var srNormalize = require('semantic-release/dist/lib/plugins').normalize;
 
 var verifyRelease = srNormalize({}, "semantic-release/dist/lib/plugin-noop");
 var analyzeCommits = require('lerna-semantic-release-analyze-commits').analyze;
-var lastReleaseNpm = srNormalize({}, "@semantic-release/last-release-npm");
+var lastReleaseNpm = require('lerna-semantic-release-get-last-release').getLastRelease;
 
 var srPre = require('semantic-release/dist/pre');
 
--- packages/lerna-semantic-release-pre/test/unit.js
+++ packages/lerna-semantic-release-pre/test/unit.js
@@ -3,7 +3,17 @@ var expect = require('expect.js');
 var path = require('path');
 var pre = require('../index');
 
-describe('pre', function() {
+function isPatchReleaseCommit (commit, packageName) {
+  var isPatchReleaseCommit = true;
+  var commitParts = commit.split('\n\n');
+
+  isPatchReleaseCommit = isPatchReleaseCommit && commitParts[0].indexOf('chore') === 0;
+  isPatchReleaseCommit = isPatchReleaseCommit && commitParts[1].indexOf('affects: ' + packageName + '@0.0.1') === 0;
+  isPatchReleaseCommit = isPatchReleaseCommit && commitParts[2].indexOf('Released from sha FOO') === 0;
+  return isPatchReleaseCommit;
+}
+
+describe('pre with three packages', function() {
   beforeEach(function () {
     var packageVersions = {
       versions: {
@@ -49,16 +59,6 @@ describe('pre', function() {
     io.restore();
   });
 
-  function isPatchReleaseCommit (commit, packageName) {
-    var isPatchReleaseCommit = true;
-    var commitParts = commit.split('\n\n');
-
-    isPatchReleaseCommit = isPatchReleaseCommit && commitParts[0].indexOf('chore') === 0;
-    isPatchReleaseCommit = isPatchReleaseCommit && commitParts[1].indexOf('affects: ' + packageName + '@0.0.1') === 0;
-    isPatchReleaseCommit = isPatchReleaseCommit && commitParts[2].indexOf('Released from sha FOO') === 0;
-    return isPatchReleaseCommit;
-  }
-
   describe('executing', function() {
     beforeEach(function (done) {
       pre({
@@ -84,6 +84,83 @@ describe('pre', function() {
       expect(isPatchReleaseCommit(io.git.commit.firstCall.args[0], 'a')).to.equal(true);
       expect(isPatchReleaseCommit(io.git.commit.secondCall.args[0], 'b')).to.equal(true);
       expect(isPatchReleaseCommit(io.git.commit.thirdCall.args[0], 'c')).to.equal(true);
+    });
+  });
+});
+
+describe('pre with a private package', function() {
+  beforeEach(function () {
+    var packageVersions = {
+      versions: {
+        'a': '1.0.0',
+      }
+    };
+
+    io.mock({
+      fs: {
+        'packages': {
+          'a': {
+            'index.js': 'Private!',
+            'package.json': JSON.stringify({name: 'a', version: '1.0.0', 'private': true})
+          }
+        },
+        'package.json': JSON.stringify({name: 'main', version: '0.0.0'}),
+        'lerna.json': JSON.stringify({lerna: '2.0.0-beta.17', version: 'independent'})
+      },
+      git: {
+        allTags: [
+          'a@1.0.0'
+        ],
+        head: 'FOO',
+        log: [{
+            message: 'fix: a\n\naffects: a\n\nThis is not a breaking change',
+            hash: 'FOO',
+            date: '2015-08-22 12:01:42 +0200'
+          },
+          {
+            message: 'fix: a\n\naffects: a\n\nBREAKING CHANGE: this is a breaking change, for testing',
+            hash: 'BREAK',
+            date: '2015-08-22 12:01:42 +0200',
+            tags: '1.0.0'
+          }
+        ]
+      },
+      npm: packageVersions,
+      lerna: packageVersions
+    });
+  });
+
+  afterEach(function () {
+    io.restore();
+  });
+
+  describe('executing', function() {
+    beforeEach(function (done) {
+      pre({
+        io: io,
+        callback: done
+      });
+    });
+
+    it('should not modify the main package.json', function () {
+      expect(JSON.parse(io.fs.readFileSync('./package.json')).version).to.equal('0.0.0');
+    });
+
+    it('should not publish at all', function () {
+      expect(io.npm.publish.innerTask.called).to.equal(false);
+    });
+
+    it('should set the npm version 1 time', function () {
+      expect(io.npm.version.innerTask.callCount).to.equal(1);
+    });
+
+    it('should make 1 git commit', function () {
+      expect(io.git.commit.innerTask.callCount).to.equal(1);
+      expect(isPatchReleaseCommit(io.git.commit.firstCall.args[0], 'a')).to.equal(true);
+    });
+
+    it('should leave the version as 1.0.1', function () {
+      expect(JSON.parse(io.fs.readFileSync('packages/a/package.json')).version).to.equal('1.0.1');
     });
   });
 });
