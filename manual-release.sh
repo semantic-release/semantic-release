5ef73e4478c9e3e44e4ee4a9ecee2ebffebd5c4b
Joshua Nelson <jonelson@atlassian.com>
2016-07-11 15:11:22 +1000

    chore(build): add manual release shell script
    useful for bootstrapping

---
+++ manual-release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -x -e
+
+LERNA=./node_modules/.bin/lerna
+$LERNA --version # will fail the script if lerna is the wrong version
+$LERNA exec -- echo 'testing to make sure lerna is installed correctly'
+
+PKG=$1
+RELATIVE_PKG=packages/$PKG
+VERSION_TYPE=$2
+
+
+git fetch
+git pull
+
+pushd $RELATIVE_PKG
+npm version $VERSION_TYPE --no-git-tag-version
+VERSION=$(node -e "console.log(require('./package.json').version)")
+popd
+
+git commit -am"chore(release): manual bump for release of $PKG@$VERSION"
+git tag $PKG@$VERSION
+
+git push --tags
+
+
+$LERNA exec -- ln -sf ../../.git .git
+npm publish packages/$PKG
+$LERNA exec -- unlink .git
