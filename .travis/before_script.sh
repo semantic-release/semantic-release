ffd2c1b3d516427ed333c6a731d513c3e72818ba
Joshua Nelson <jonelson@atlassian.com>
2016-09-02 15:45:40 -0700

    chore(debug): branch yet to be born problem in prev commit

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -15,7 +15,7 @@ if [[ $TRAVIS_BRANCH == 'caribou' ]]; then
   git fetch origin
   git checkout
   git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
-  git checkout $TRAVIS_BRANCH
+  git checkout caribou
 
   git config credential.helper store
   echo "https://lerna-sr-travis-ci:${RELEASE_GH_TOKEN}@github.com/atlassian/lerna-semantic-release.git" > ~/.git-credentials
