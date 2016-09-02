65891a249a72758c2e4f11d4a4c0dcd795ce736c
Joshua Nelson <jonelson@atlassian.com>
2016-09-01 23:44:25 -0700

    chore(debug): debug

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -8,12 +8,11 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 
 if [[ $TRAVIS_BRANCH == 'caribou' ]]; then
+  ls -la
   git config credential.helper store
   echo "https://lerna-sr-travis-ci:${RELEASE_GH_TOKEN}@github.com/atlassian/lerna-semantic-release.git" > ~/.git-credentials
-  cd ../..
   rm -rf $TRAVIS_REPO_SLUG
   git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
-  cd $TRAVIS_REPO_SLUG
 
   npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
   npm prune
