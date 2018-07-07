3003c51ecee27ffada0805cd2de8f82177d6226b
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-07-07 18:25:33 -0400

    fix: allow empty release notes in dry-run mode

--- index.js
+++ index.js
@@ -103,7 +103,9 @@ async function run(options, plugins) {
     logger.log('Call plugin %s', 'generate-notes');
     const notes = await plugins.generateNotes(generateNotesParam);
     logger.log('Release note for version %s:\n', nextRelease.version);
-    process.stdout.write(`${marked(notes)}\n`);
+    if (notes) {
+      process.stdout.write(`${marked(notes)}\n`);
+    }
   } else {
     logger.log('Call plugin %s', 'generateNotes');
     nextRelease.notes = await plugins.generateNotes(generateNotesParam);
