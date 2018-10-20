df795661f0061233388eeb94167bd82467d4251a
tsirlucas <tsirlucas@gmail.com>
2018-10-20 10:16:38 -0300

    feat: Return release notes on nextRelease even if running on dry mode

--- index.js
+++ index.js
@@ -93,17 +93,16 @@ async function run(context, plugins) {
   context.nextRelease = nextRelease;
   nextRelease.version = getNextVersion(context);
   nextRelease.gitTag = template(options.tagFormat)({version: nextRelease.version});
+  nextRelease.notes = await plugins.generateNotes(context);
 
   await plugins.verifyRelease(context);
 
   if (options.dryRun) {
-    const notes = await plugins.generateNotes(context);
     logger.log(`Release note for version ${nextRelease.version}:`);
-    if (notes) {
-      context.stdout.write(marked(notes));
+    if (nextRelease.notes) {
+      context.stdout.write(marked(nextRelease.notes));
     }
   } else {
-    nextRelease.notes = await plugins.generateNotes(context);
     await plugins.prepare(context);
 
     // Create the tag before calling the publish plugins as some require the tag to exists
