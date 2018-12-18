a7d452f032ca55c7967ce801b7e952b2beff1024
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-12-18 01:53:09 -0500

    fix: do not call `addChannel`for 2 merged branches configured with the same channel

--- test/get-releases-to-add.test.js
+++ test/get-releases-to-add.test.js
@@ -63,7 +63,7 @@ test('Return versions merged from release to maintenance branch, excluding lower
   ]);
 });
 
-test('Return versions merged from future branch to release branch', t => {
+test('Return versions merged between release branches', t => {
   const result = getReleasesToAdd({
     branch: {
       name: 'master',
@@ -177,7 +177,7 @@ test('Return releases sorted by ascending order', t => {
   ]);
 });
 
-test('no lastRelease', t => {
+test('No lastRelease', t => {
   const result = getReleasesToAdd({
     branch: {name: 'master', tags: [{gitTag: 'v1.0.0@next', version: '1.0.0', channel: 'next', gitHead: '111'}]},
     branches: [{name: 'master'}, {name: 'next', channel: 'next'}],
@@ -247,4 +247,67 @@ test('Ignore pre-release versions', t => {
       },
     },
   ]);
+});
+
+test('Exclude versions merged from release to maintenance branch if they have the same "channel"', t => {
+  const result = getReleasesToAdd({
+    branch: {
+      name: '2.x',
+      channel: 'latest',
+      type: 'maintenance',
+      'merge-range': '>=2.0.0 <3.0.0',
+      tags: [
+        {gitTag: 'v2.0.0', version: '2.0.0', gitHead: '111'},
+        {gitTag: 'v2.0.0', version: '2.0.0', gitHead: '111'},
+        {gitTag: 'v2.1.0', version: '2.1.0', gitHead: '222'},
+        {gitTag: 'v2.1.1', version: '2.1.1', gitHead: '333'},
+        {gitTag: 'v1.0.0', version: '1.0.0', gitHead: '444'},
+        {gitTag: 'v1.1.0', version: '1.1.0', gitHead: '555'},
+      ],
+    },
+    branches: [{name: '2.x', channel: 'latest'}, {name: 'master', channel: 'latest'}],
+    options: {tagFormat: `v\${version}`},
+  });
+
+  t.deepEqual(result, []);
+});
+
+test('Exclude versions merged between release branches if they have the same "channel"', t => {
+  const result = getReleasesToAdd({
+    branch: {
+      name: 'master',
+      channel: 'latest',
+      tags: [
+        {gitTag: 'v1.0.0', channel: 'latest', version: '1.0.0', gitHead: '111'},
+        {gitTag: 'v1.1.0', channel: 'latest', version: '1.1.0', gitHead: '222'},
+        {gitTag: 'v2.0.0', channel: 'latest', version: '2.0.0', gitHead: '333'},
+      ],
+    },
+    branches: [
+      {name: 'master', channel: 'latest'},
+      {name: 'next', channel: 'latest'},
+      {name: 'next-major', channel: 'latest'},
+    ],
+    options: {tagFormat: `v\${version}`},
+  });
+
+  t.deepEqual(result, []);
+});
+
+test('Exclude versions merged between release branches if they all have "channel" set to "false"', t => {
+  const result = getReleasesToAdd({
+    branch: {
+      name: 'master',
+      channel: false,
+      tags: [
+        {gitTag: 'v1.0.0', version: '1.0.0', gitHead: '111'},
+        {gitTag: 'v1.1.0', version: '1.1.0', gitHead: '222'},
+        {gitTag: 'v2.0.0', version: '2.0.0', gitHead: '333'},
+      ],
+    },
+    branches: [{name: 'master', channel: false}, {name: 'next', channel: false}, {name: 'next-major', channel: false}],
+    options: {tagFormat: `v\${version}`},
+  });
+
+  t.deepEqual(result, []);
 });
