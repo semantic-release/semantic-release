6b3adf6bbe72d5c32335118e91ca32ec043bbc9e
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2019-06-26 12:11:47 -0400

    fix: revert to execa `^1.0.0`

--- test/integration.test.js
+++ test/integration.test.js
@@ -72,9 +72,9 @@ test('Release patch, minor and major versions', async t => {
   t.log('Commit a chore');
   await gitCommits(['chore: Init repository'], {cwd});
   t.log('$ semantic-release');
-  let {stdout, exitCode} = await execa(cli, [], {env, cwd});
+  let {stdout, code} = await execa(cli, [], {env, cwd});
   t.regex(stdout, /There are no relevant changes, so no new version is released/);
-  t.is(exitCode, 0);
+  t.is(code, 0);
 
   /* Initial release */
   let version = '1.0.0';
@@ -95,10 +95,10 @@ test('Release patch, minor and major versions', async t => {
   t.log('Commit a feature');
   await gitCommits(['feat: Initial commit'], {cwd});
   t.log('$ semantic-release');
-  ({stdout, exitCode} = await execa(cli, [], {env, cwd}));
+  ({stdout, code} = await execa(cli, [], {env, cwd}));
   t.regex(stdout, new RegExp(`Published GitHub release: release-url/${version}`));
   t.regex(stdout, new RegExp(`Publishing version ${version} to npm registry`));
-  t.is(exitCode, 0);
+  t.is(code, 0);
 
   // Verify package.json and npm-shrinkwrap.json have been updated
   t.is((await readJson(path.resolve(cwd, 'package.json'))).version, version);
@@ -137,10 +137,10 @@ test('Release patch, minor and major versions', async t => {
   t.log('Commit a fix');
   await gitCommits(['fix: bar'], {cwd});
   t.log('$ semantic-release');
-  ({stdout, exitCode} = await execa(cli, [], {env, cwd}));
+  ({stdout, code} = await execa(cli, [], {env, cwd}));
   t.regex(stdout, new RegExp(`Published GitHub release: release-url/${version}`));
   t.regex(stdout, new RegExp(`Publishing version ${version} to npm registry`));
-  t.is(exitCode, 0);
+  t.is(code, 0);
 
   // Verify package.json and npm-shrinkwrap.json have been updated
   t.is((await readJson(path.resolve(cwd, 'package.json'))).version, version);
@@ -179,10 +179,10 @@ test('Release patch, minor and major versions', async t => {
   t.log('Commit a feature');
   await gitCommits(['feat: baz'], {cwd});
   t.log('$ semantic-release');
-  ({stdout, exitCode} = await execa(cli, [], {env, cwd}));
+  ({stdout, code} = await execa(cli, [], {env, cwd}));
   t.regex(stdout, new RegExp(`Published GitHub release: release-url/${version}`));
   t.regex(stdout, new RegExp(`Publishing version ${version} to npm registry`));
-  t.is(exitCode, 0);
+  t.is(code, 0);
 
   // Verify package.json and npm-shrinkwrap.json have been updated
   t.is((await readJson(path.resolve(cwd, 'package.json'))).version, version);
@@ -221,10 +221,10 @@ test('Release patch, minor and major versions', async t => {
   t.log('Commit a breaking change');
   await gitCommits(['feat: foo\n\n BREAKING CHANGE: bar'], {cwd});
   t.log('$ semantic-release');
-  ({stdout, exitCode} = await execa(cli, [], {env, cwd}));
+  ({stdout, code} = await execa(cli, [], {env, cwd}));
   t.regex(stdout, new RegExp(`Published GitHub release: release-url/${version}`));
   t.regex(stdout, new RegExp(`Publishing version ${version} to npm registry`));
-  t.is(exitCode, 0);
+  t.is(code, 0);
 
   // Verify package.json and npm-shrinkwrap.json have been updated
   t.is((await readJson(path.resolve(cwd, 'package.json'))).version, version);
@@ -258,8 +258,8 @@ test('Exit with 1 if a plugin is not found', async t => {
     release: {analyzeCommits: 'non-existing-path', success: false, fail: false},
   });
 
-  const {exitCode, stderr} = await t.throwsAsync(execa(cli, [], {env, cwd}));
-  t.is(exitCode, 1);
+  const {code, stderr} = await t.throwsAsync(execa(cli, [], {env, cwd}));
+  t.is(code, 1);
   t.regex(stderr, /Cannot find module/);
 });
 
@@ -276,8 +276,8 @@ test('Exit with 1 if a shareable config is not found', async t => {
     release: {extends: 'non-existing-path', success: false, fail: false},
   });
 
-  const {exitCode, stderr} = await t.throwsAsync(execa(cli, [], {env, cwd}));
-  t.is(exitCode, 1);
+  const {code, stderr} = await t.throwsAsync(execa(cli, [], {env, cwd}));
+  t.is(code, 1);
   t.regex(stderr, /Cannot find module/);
 });
 
@@ -297,8 +297,8 @@ test('Exit with 1 if a shareable config reference a not found plugin', async t =
   });
   await writeJson(path.resolve(cwd, 'shareable.json'), shareable);
 
-  const {exitCode, stderr} = await t.throwsAsync(execa(cli, [], {env, cwd}));
-  t.is(exitCode, 1);
+  const {code, stderr} = await t.throwsAsync(execa(cli, [], {env, cwd}));
+  t.is(code, 1);
   t.regex(stderr, /Cannot find module/);
 });
 
@@ -327,11 +327,11 @@ test('Dry-run', async t => {
   t.log('Commit a feature');
   await gitCommits(['feat: Initial commit'], {cwd});
   t.log('$ semantic-release -d');
-  const {stdout, exitCode} = await execa(cli, ['-d'], {env, cwd});
+  const {stdout, code} = await execa(cli, ['-d'], {env, cwd});
   t.regex(stdout, new RegExp(`There is no previous release, the next release version is ${version}`));
   t.regex(stdout, new RegExp(`Release note for version ${version}`));
   t.regex(stdout, /Initial commit/);
-  t.is(exitCode, 0);
+  t.is(code, 0);
 
   // Verify package.json and has not been modified
   t.is((await readJson(path.resolve(cwd, 'package.json'))).version, '0.0.0-dev');
@@ -375,10 +375,10 @@ test('Allow local releases with "noCi" option', async t => {
   t.log('Commit a feature');
   await gitCommits(['feat: Initial commit'], {cwd});
   t.log('$ semantic-release --no-ci');
-  const {stdout, exitCode} = await execa(cli, ['--no-ci'], {env: envNoCi, cwd});
+  const {stdout, code} = await execa(cli, ['--no-ci'], {env: envNoCi, cwd});
   t.regex(stdout, new RegExp(`Published GitHub release: release-url/${version}`));
   t.regex(stdout, new RegExp(`Publishing version ${version} to npm registry`));
-  t.is(exitCode, 0);
+  t.is(code, 0);
 
   // Verify package.json and has been updated
   t.is((await readJson(path.resolve(cwd, 'package.json'))).version, version);
@@ -417,7 +417,7 @@ test('Pass options via CLI arguments', async t => {
   t.log('Commit a feature');
   await gitCommits(['feat: Initial commit'], {cwd});
   t.log('$ semantic-release');
-  const {stdout, exitCode} = await execa(
+  const {stdout, code} = await execa(
     cli,
     [
       '--verify-conditions',
@@ -433,7 +433,7 @@ test('Pass options via CLI arguments', async t => {
     {env, cwd}
   );
   t.regex(stdout, new RegExp(`Publishing version ${version} to npm registry`));
-  t.is(exitCode, 0);
+  t.is(code, 0);
 
   // Verify package.json and has been updated
   t.is((await readJson(path.resolve(cwd, 'package.json'))).version, version);
@@ -528,14 +528,14 @@ test('Log unexpected errors from plugins and exit with 1', async t => {
   t.log('Commit a feature');
   await gitCommits(['feat: Initial commit'], {cwd});
   t.log('$ semantic-release');
-  const {stderr, exitCode} = await execa(cli, [], {env, cwd, reject: false});
+  const {stderr, code} = await execa(cli, [], {env, cwd, reject: false});
   // Verify the type and message are logged
   t.regex(stderr, /Error: a/);
   // Verify the the stacktrace is logged
   t.regex(stderr, new RegExp(pluginError));
   // Verify the Error properties are logged
   t.regex(stderr, /errorProperty: 'errorProperty'/);
-  t.is(exitCode, 1);
+  t.is(code, 1);
 });
 
 test('Log errors inheriting SemanticReleaseError and exit with 1', async t => {
@@ -555,10 +555,10 @@ test('Log errors inheriting SemanticReleaseError and exit with 1', async t => {
   t.log('Commit a feature');
   await gitCommits(['feat: Initial commit'], {cwd});
   t.log('$ semantic-release');
-  const {stderr, exitCode} = await execa(cli, [], {env, cwd, reject: false});
+  const {stderr, code} = await execa(cli, [], {env, cwd, reject: false});
   // Verify the type and message are logged
   t.regex(stderr, /EINHERITED Inherited error/);
-  t.is(exitCode, 1);
+  t.is(code, 1);
 });
 
 test('Exit with 1 if missing permission to push to the remote repository', async t => {
@@ -573,14 +573,14 @@ test('Exit with 1 if missing permission to push to the remote repository', async
   await gitCommits(['feat: Initial commit'], {cwd});
   await gitPush('origin', 'master', {cwd});
   t.log('$ semantic-release');
-  const {stderr, exitCode} = await execa(
+  const {stderr, code} = await execa(
     cli,
     ['--repository-url', 'http://user:wrong_pass@localhost:2080/git/unauthorized.git'],
     {env: {...env, GH_TOKEN: 'user:wrong_pass'}, cwd, reject: false}
   );
   // Verify the type and message are logged
   t.regex(stderr, /EGITNOPERMISSION/);
-  t.is(exitCode, 1);
+  t.is(code, 1);
 });
 
 test('Hide sensitive environment variable values from the logs', async t => {
