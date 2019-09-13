0629f3cd8af8068a0f6daedc1312c37d1936d87f
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2019-09-13 14:46:30 -0400

    revert: feat: create annotated tags
    This reverts commit 4d581fc140dda99065542872d125cf27fb24798f.

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ async function run(context, plugins) {
     nextRelease.notes = await plugins.generateNotes({...context, commits, lastRelease, nextRelease});
 
     logger.log('Create tag %s', nextRelease.gitTag);
-    await tag(nextRelease, {cwd, env});
+    await tag(nextRelease.gitTag, nextRelease.gitHead, {cwd, env});
     await push(options.repositoryUrl, {cwd, env});
     context.branch.tags.push({
       version: nextRelease.version,
@@ -170,7 +170,7 @@ async function run(context, plugins) {
     logger.warn(`Skip ${nextRelease.gitTag} tag creation in dry-run mode`);
   } else {
     // Create the tag before calling the publish plugins as some require the tag to exists
-    await tag(nextRelease, {cwd, env});
+    await tag(nextRelease.gitTag, nextRelease.gitHead, {cwd, env});
     await push(options.repositoryUrl, {cwd, env});
     logger.success(`Created tag ${nextRelease.gitTag}`);
   }
